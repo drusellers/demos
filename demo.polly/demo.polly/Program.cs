b4569ddc2102dd7fbf08ee40a4f2a6790abe7772
Dru Sellers <dru@drusellers.com>
2016-09-10 08:10:02 -0500

    polly play

--- demo.polly/demo.polly/Program.cs
+++ demo.polly/demo.polly/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Sockets;
+using Autofac;
+using Autofac.Core;
 using Polly;
 using Polly.CircuitBreaker;
 
@@ -16,8 +19,8 @@ namespace demo.polly
 
             //QUESTION: How can i setup RETRY and CIRCUIT BREAKER
             //          Retry 3 times, then break for 10 minutes
-            var policy = BreakCircuitAfterOneError();
-
+            //var p = Retry();
+            var p = BreakCircuitAfterOneError();
 
 
             //simulate external callers
@@ -40,6 +43,58 @@ namespace demo.polly
             Console.ReadLine();
 
         }
+
+        void Explore()
+        {
+            var cb = new ContainerBuilder();
+
+            cb.Register(cxt =>
+            {
+                return Policy.Handle<Exception>()
+                    .CircuitBreaker(1, TimeSpan.FromSeconds(60));
+            })
+            .AsSelf()
+            .Named<Policy>("abc")
+            .SingleInstance();
+
+            //wrap this in WrapperService
+            cb.RegisterType<RealService>()
+                .As<Service>()
+
+                ;
+        }
+
+
+        public class RealService : Service
+        {
+            Policy _p = Policy.Handle<Exception>()
+                .Retry(5);
+
+            public RealService(Policy p)
+            {
+                _p = p;
+            }
+
+            public int DoIt()
+            {
+
+                var r = _p.ExecuteAndCapture(() =>
+                {
+
+                });
+
+                r.
+                return 1;
+
+            }
+        }
+
+
+        public interface Service
+        {
+            int DoIt();
+        }
+
 
 
         private static Policy BreakCircuitAfterOneError()
