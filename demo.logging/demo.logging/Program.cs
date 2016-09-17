f261ae34d876d472208a9de981f2cad0a007441f
Dru Sellers <dru@drusellers.com>
2016-09-16 22:47:11 -0500

    playing with logging levels

--- demo.logging/demo.logging/Program.cs
+++ demo.logging/demo.logging/Program.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Serilog;
 using Serilog.Events;
+using Serilog.Formatting.Json;
 using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.RollingFile;
 
 namespace demo.logging
 {
@@ -14,15 +16,36 @@ namespace demo.logging
         static void Main(string[] args)
         {
             Serilog.LoggerConfiguration cfg = new LoggerConfiguration()
-                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
-                {
-                    AutoRegisterTemplate = true,
-                    BufferBaseFilename = @"C:\temp\elk",
-                    BufferLogShippingInterval = TimeSpan.FromSeconds(5),
-                    MinimumLogEventLevel = LogEventLevel.Verbose
-                });
+//                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+//                {
+//                    AutoRegisterTemplate = true,
+//                    BufferBaseFilename = @"C:\temp\elk",
+//                    BufferLogShippingInterval = TimeSpan.FromSeconds(5),
+//                    MinimumLogEventLevel = LogEventLevel.Verbose
+//                })
+                .WriteTo.Console(LogEventLevel.Information)
+                .WriteTo.Sink(new RollingFileSink("log.txt",new JsonFormatter(), 2097152, 5, Encoding.UTF8 ))
+                ;
 
-            cfg.CreateLogger().Information("HI");
+            var logger = cfg.CreateLogger();
+
+
+            logger.Debug("HI:");
+            logger.Verbose("HI:");
+            logger.Information("HI:");
+            logger.Error("HI:");
+            logger.Warning("HI:");
+            logger.Fatal("HI:");
+
+            var x = $@"Debug: {logger.IsEnabled(LogEventLevel.Debug)}
+Verbose: {logger.IsEnabled(LogEventLevel.Verbose)}
+Info: {logger.IsEnabled(LogEventLevel.Information)}
+Error: {logger.IsEnabled(LogEventLevel.Error)}
+Warning: {logger.IsEnabled(LogEventLevel.Warning)}
+Fatal: {logger.IsEnabled(LogEventLevel.Fatal)}";
+
+            logger.Fatal(x);
+
             Console.ReadKey(true);
         }
     }
